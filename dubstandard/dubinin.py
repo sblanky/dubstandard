c28779fdbe0f02719424cc293414a466f8ce1755
L. Scott Blankenship <leo.blankenship1@nottingham.ac.uk>
2023-05-17 17:05:36 +0100

    matrices work

--- dubstandard/dubinin.py
+++ dubstandard/dubinin.py
@@ -1,16 +1,11 @@
-import matplotlib.pyplot as plt
 from scipy.interpolate import PchipInterpolator
 from scipy import optimize, stats, constants
 import numpy as np
-from collections import OrderedDict
-import pandas as pd
-import os
-from pprint import pprint
-from pathlib import Path
+
+import warnings
 
 import pygaps.parsing as pgp
 import pygaps.characterisation as pgc
-import pygaps.graphing.calc_graphs as pgraph
 from pygaps.utilities.exceptions import CalculationError
 
 
@@ -36,6 +31,9 @@ class DubininResult:
         exp: float = None,
         **kwargs,
     ):
+        def zero_matrix(dim):
+            return np.zeros([dim, dim])
+
         self.material = isotherm.material
         self.adsorbate = isotherm.adsorbate
         self.iso_temp = isotherm.temperature
@@ -56,7 +54,20 @@ class DubininResult:
             material_basis='mass',
         )
 
-        self.total_pore_volume = max(self.loading) * self.molar_mass / self.liquid_density
+        self.plateau_loading = isotherm.loading_at(
+            pressure=0.9,
+            branch='ads',
+            pressure_mode='relative',
+            loading_unit='mol',
+            loading_basis='molar',
+            material_unit='g',
+            material_basis='mass',
+        )
+
+        self.total_pore_volume = (
+            self.plateau_loading * self.molar_mass /
+            self.liquid_density
+        )
 
         self.log_v = pgc.dr_da_plots.log_v_adj(
             self.loading,
@@ -64,6 +75,24 @@ class DubininResult:
             self.liquid_density,
         )
 
+        num_points = len(self.pressure)
+
+        self.point_count = zero_matrix(num_points)
+        self.fit_grad = zero_matrix(num_points)
+        self.fit_intercept = zero_matrix(num_points)
+        self.fit_rsquared = zero_matrix(num_points)
+        self.p_val = zero_matrix(num_points)
+        self.stderr = zero_matrix(num_points)
+        self.second_derivative = zero_matrix(num_points)
+
+        self.potential = zero_matrix(num_points)
+        self.pore_volume = zero_matrix(num_points)
+
+        self.pressure_max = zero_matrix(num_points)
+        self.pressure_min = zero_matrix(num_points)
+
+        self.filter_volume = zero_matrix(num_points)
+        self.filter_pressure = zero_matrix(num_points)
 
         def dr_fit(exp, ret=False):
             slope, intercept, corr_coef, _, stderr = stats.linregress(
@@ -114,59 +143,42 @@ class DubininResult:
         num_points = len(pressure)
         self.result = {}
         for i in range(num_points):
-            for j in range(i+3, num_points):
-                self.result[i, j] = {}
-                pressure_range = [
-                    self.pressure[i],
-                    self.pressure[j]
-                ]
+            for j in range(i+1, num_points):
+                self.point_count[i, j] = j - i
 
                 try:
-                    (
-                        slope,
-                        intercept,
-                        corr_coef,
-                        p_val,
-                        stderr,
-                    ) = stats.linregress(
-                        self.log_p_exp[i:j],
-                        self.log_v[i:j]
-                    )
-                    microp_volume = 10**intercept
-                    potential = (-np.log(10)**(self.exp - 1) *
-                                (constants.gas_constant * self.iso_temp)**(self.exp) \
-                                 / slope)**(1 / self.exp) / 1000
-                    self.result[i, j] = {
-                        'microp_volume': microp_volume,
-                        'potential': potential,
-                        'slope': slope,
-                        'intercept': intercept,
-                        'corr_coef': corr_coef,
-                        'min_p_index': i,
-                        'max_p_index': j,
-                    }
+                    self.pressure_min[i, j] = self.pressure[i]
+                    self.pressure_min[i, j] = self.pressure[j]
 
-                    self.result[i, j]['point_count'] = j - i
-                    self.result[i, j]['pressure_range'] = pressure_range
+                    with warnings.catch_warnings(record=True) as w:
+                        (
+                            fit_grad,
+                            fit_intercept,
+                            fit_rvalue,
+                            p_val,
+                            stderr,
+                        ) = stats.linregress(
+                            self.log_p_exp[i:j],
+                            self.log_v[i:j]
+                        )
+                        if len(w) > 0:
+                            continue
+
+                    self.fit_grad[i, j] = fit_grad
+                    self.fit_intercept[i, j] = fit_intercept
+                    self.fit_rsquared[i, j] = fit_rvalue**2
+                    self.p_val[i, j] = p_val
+                    self.stderr[i, j] = stderr
+
+                    self.pore_volume[i, j] = 10**fit_intercept
+                    self.potential[i, j] = (
+                        (-np.log(10)**(self.exp - 1) *
+                         (constants.gas_constant * self.iso_temp)**(self.exp)
+                         / fit_grad)**(1 / self.exp) / 1000
+                    )
 
                 except CalculationError as e:
                     print(e)
-                    continue
-
-                log_p_exp = self.log_p_exp[i:j]
-                try:
-                    derivs = [
-                       min(second_derivative(log_p_exp)),
-                       max(second_derivative(log_p_exp))
-                    ]
-                    deriv_change = abs(derivs[1]-derivs[0])
-                    x_range = abs(log_p_exp[0]-log_p_exp[-1])
-                    relative_change = deriv_change / np.log10(x_range)
-                    self.result[i, j]['curvature'] = abs(relative_change)
-                except ValueError as e:
-                    print(e)
-                    continue
-                except UnboundLocalError as e:
                     continue
 
 
@@ -183,235 +195,34 @@ class DubininFilteredResults:
         **kwargs,
     ):
         self.__dict__.update(dubinin_result.__dict__)
-
-        while len(self.result) != 0:
-            p_limits = kwargs.get('p_limits', [0, 0.1])
-            fail_point = 'p_limits'
-            self._filter(
-                'pressure_range',
-                lambda x: x[0] < p_limits[0] or x[1] > p_limits[1]
-            )
-
-            curvature_limit = kwargs.get('curvature_limit', 0.02)
-            fail_point = 'curvature_limit'
-            self._filter(
-                'curvature',
-                lambda x: abs(x) > curvature_limit
-            )
-
-            max_volume = kwargs.get(
-                'max_volume', self.total_pore_volume
-            )
-            fail_point = 'max_volume'
-            self._filter(
-                'microp_volume',
-                lambda x: x > max_volume
-            )
-
-            min_points = kwargs.get('min_points', 5)
-            fail_point = 'min_points'
-            self._filter(
-                'point_count',
-                lambda x: x < min_points
-            )
-
-            min_corr_coef = kwargs.get('min_corr_coef', 0.95)
-            fail_point = 'min_corr_coef'
-            self._filter(
-                'corr_coef',
-                lambda x: abs(x) < min_corr_coef
-            )
-
-            fail_point = 'SUCCESS'
-            break
-
         self.filter_params = kwargs
 
-        if len(self.result) == 0:
-            raise ValueError(
-                f'No valid results with applied filters, '
-                f'failed with to "{fail_point}". Consider '
-                f'loosening the parameters.'
+        filter_mask = np.ones_like(dubinin_result.point_count)
+
+        min_points = kwargs.get('min_points', 10)
+        filter_mask = filter_mask * (dubinin_result.point_count > min_points)
+
+        min_r2 = kwargs.get('min_r2', 0.99)
+        filter_mask = filter_mask * (dubinin_result.fit_rsquared > min_r2)
+
+        max_volume = kwargs.get('max_volume', dubinin_result.total_pore_volume)
+        filter_mask = filter_mask * (dubinin_result.fit_rsquared < max_volume)
+
+        p_limits = kwargs.get('p_limits', [1e-5, 0.001])
+        filter_mask = filter_mask * (dubinin_result.pressure_min > p_limits[0])
+        filter_mask = filter_mask * (dubinin_result.pressure_max < p_limits[1])
+
+        self.has_valid_volumes = False
+        if np.sum(filter_mask) != 0:
+            self.has_valid_volumes = True
+            self.pore_volume_filtered = self.pore_volume * filter_mask
+            self.valid_indices = np.where(
+                self.pore_volume_filtered > 0
             )
+            self.num_valid = len(self.valid_indices[0])
+            self.valid_volumes = self.pore_volume_filtered[self.valid_indices]
 
-        self._stats()
-
-        self._optimum(optimum_criteria)
-
-    def _optimum(
-        self,
-        optimum_criteria,
-    ):
-
-        if optimum_criteria not in [
-            'min_volume',
-            'max_points',
-            'max_corr_coef'
-        ]:
-            raise ValueError(
-                f'{optimum_criteria} is invalid selection '
-                f'criterion for selection of optimum Dubinin '
-                f'volume.'
-            )
-
-        if optimum_criteria == 'min_volume':
-            optimum = 1e10
-            for r in self.result:
-                if self.result[r]['microp_volume'] < optimum:
-                    optimum = self.result[r]['microp_volume']
-                    self.optimum = self.result[r]
-
-        if optimum_criteria == 'max_points':
-            optimum = 0
-            for r in self.result:
-                if self.result[r]['point_count'] > optimum:
-                    optimum = self.result[r]['point_count']
-                    self.optimum = self.result[r]
-
-        if optimum_criteria == 'max_corr_coef':
-            optimum = 0
-            for r in self.result:
-                if abs(self.result[r]['corr_coef']) > optimum:
-                    optimum = self.result[r]['corr_coef']
-                    self.optimum = self.result[r]
-
-        self.optimum_criteria = optimum_criteria
-
-    def _filter(
-        self,
-        key,
-        criteria,
-    ):
-        """
-        Remove results based on filter criteria. Criteria given
-        in the form of lambda expression.
-        """
-        if len(self.result) == 0:
-            print(
-                f'{self.material}:\n'
-                f'Nothing to filter'
-            )
-        to_remove = []
-        for r in self.result:
-            try:
-                result = self.result[r]
-                if criteria(result[key]):
-                    to_remove.append(r)
-            except KeyError:
-                pass
-
-        for r in to_remove:
-            del self.result[r]
-
-    def _sort(self):
-        """
-        Sort results (assumedly filtered) by `point_count` and
-        `corr_coef`.
-        TODO: make output in a reasonable format.
-        """
-        self.result = OrderedDict(sorted(
-            self.result.items(),
-            key=lambda x: (x[1]['point_count'], x[1]['corr_coef'])
-        )
-        )
-
-    def _stats(self):
-        """
-        Determines mean and standard deviation of filtered volumes.
-        """
-        volumes = [
-            x['microp_volume'] for x in self.result.values()
-        ]
-        self.mean = np.mean(volumes)
-        self.stddev = np.std(volumes)
-
-    def plot_optimum(
-        self,
-        ax=None,
-        verbose=False,
-    ):
-        if ax is None:
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-
-        ax.scatter(
-            self.log_p_exp,
-            self.log_v,
-            label='all data',
-            ec='grey', fc='none',
-            marker='o',
-        )
-
-        idxmin = self.optimum['min_p_index']
-        idxmax = self.optimum['max_p_index']
-        ax.scatter(
-            self.log_p_exp[idxmin:idxmax],
-            self.log_v[idxmin:idxmax],
-            color='r', marker='o',
-            label='fit data',
-        )
-
-        x = np.linspace(
-            min(self.log_p_exp),
-            max(self.log_p_exp),
-            100
-        )
-        y = self.optimum['slope'] * x + self.optimum['intercept']
-
-        ax.plot(
-            x, y,
-            label='fit', color='grey', linestyle='dashed'
-        )
-        ax.legend(
-            title=f'exp: {self.exp:.2f}'
-        )
-
-        if verbose:
-            plt.show()
-
-        return fig, ax
-
-
-    def export(
-        self,
-        filepath,
-        verbose=False,
-    ):
-        """
-        Exports summary of results
-        """
-        if not os.path.exists(filepath):
-            os.makedirs(filepath)
-
-        with (Path(f'{filepath}filter_summary.json')).open('w') as fp:
-            pprint(self.filter_params, fp)
-
-        with (Path(f'{filepath}optimum.txt')).open('w') as fp:
-            print(
-                f"Optimum volume selected using {self.optimum_criteria}: \n"
-                f"Pore volume: {self.optimum['microp_volume']}\n"
-                f"Points: {self.optimum['point_count']}\n"
-                f"Potential: {self.optimum['potential']}\n"
-                f"Slope: {self.optimum['slope']}\n"
-                f"Intercept: {self.optimum['intercept']}\n"
-                f"Correlation coefficent: {self.optimum['corr_coef']}\n"
-                f"Exponent: {self.exp}\n"
-                f"Pressure range: "
-                f"{self.optimum['pressure_range'][0]} - "
-                f"{self.optimum['pressure_range'][1]}\n",
-                file=fp)
-
-        pd.DataFrame(self.result).transpose().to_csv(
-            f'{filepath}filtered_results.csv',
-            index=False
-        )
-
-        fig, ax = self.plot_optimum(verbose=verbose)
-
-        if verbose:
-            plt.show()
-        fig.savefig(f'{filepath}optimum.png')
-
+            self.stdev_volume = np.std(self.valid_volumes)
 
 def analyseDA(
     isotherm,
@@ -436,7 +247,6 @@ def analyseDA(
         optimum_criteria=optimum_criteria,
         **kwargs
     )
-    filtered.plot_optimum(verbose=verbose)
 
     return filtered
 
@@ -459,7 +269,8 @@ def analyseDR(
 
 
 if __name__ == "__main__":
-    inPath = '/home/pcxtsbl/CodeProjects/labcore_upload/robert/aif/'
-    file = 'ACC2600.aif'
+    inPath = '../example/aif/'
+    file = 'Zeolite-13X.aif'
+    print(f'{inPath}{file}')
     isotherm = pgp.isotherm_from_aif(f'{inPath}{file}')
     analyseDR(isotherm, verbose=True,) 
